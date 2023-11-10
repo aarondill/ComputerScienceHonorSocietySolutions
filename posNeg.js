7aeb4876a4ff74a62a49d426491828ef0457589b
aarondill <aaronsacks2006@gmail.com>
2023-11-09 23:09:35 -0600

    change posNex assert impl

--- posNeg.js
+++ posNeg.js
@@ -3,31 +3,56 @@
 // Given two numbers x,y and a boolean a, return true if a is true and the sign of x and y are different,
 // else if a is false return true if x and y are both negative, else return false.
 
-const posNeg = (x, y, a) => !!(!a ? (x < 0) ^ (y < 0) : x < 0 && y < 0);
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {boolean} [a]
+ * @return {boolean}
+ */
+// prettier-ignore
+const posNeg = (x, y, a) => !!(a ? x < 0 && y < 0 : (x < 0) !== (y < 0));
 // const posNeg = (_, $, _$) => (($_ = _ => _ != !{}), (_ = !(+!![] + +_ > +![])), ($ = !(++$ > +![])), $_(_$ ? (_ &= $) : (_ ^= $)));
 
 // Test cases -- Run the program in node or the browser to test it.
-const assert = (v, f, ...args) => {
-  const real = f(...args);
+
+/**
+ * @template {unknown[]} A
+ * @template R
+ * @param {R} v
+ * @param {(...arg: A)=>R} f
+ * @param {A} args
+ * @return {boolean}
+ */
+const assert_eq = (v, f, ...args) => {
+  const real = f.apply(undefined, args);
   const name = f.name || "f";
-  const msg = `Test case: ${name}(${args.join()})\t=> ${v}:\t`;
-  if (real != v) {
-    if (globalThis.process) globalThis.process.exitCode = 1;
-    console.error(msg + "Failed! Returned: " + real);
-  } else console.log(msg + "Passed!");
-  return real == v;
+  const msg = `Test case: ${name}(${args.join()})\t=> ${v}:`;
+  if (real === v) {
+    console.log(`${msg}\tPassed!`);
+    return true;
+  }
+
+  console.error(`${msg}\tFailed! Returned: ${real}`);
+  if (globalThis.process) {
+    globalThis.process.exitCode = 1; // If running in node, set exit code to 1
+  }
+  return false;
 };
+
 // Example cases
-assert(true, posNeg, -1, 1, false);
-assert(true, posNeg, -1, 1, false);
-assert(true, posNeg, -4, -5, true);
+assert_eq(true, posNeg, -1, 1, false);
+assert_eq(true, posNeg, -1, 1, false);
+assert_eq(true, posNeg, -4, -5, true);
 // Additional cases
-assert(true, posNeg, -3, 2, false); // non 1
-assert(true, posNeg, -2, 3, false); // non 1
-assert(false, posNeg, -1, -1, false); // both negative
-assert(false, posNeg, 1, 1, false); // both positive
-assert(false, posNeg, -2, -3, false); // both negative-non 1
-assert(false, posNeg, 2, 3, false); // both positive-non 1
-assert(false, posNeg, 4, 5, true); // boolean true-both positve
-assert(false, posNeg, 4, -5, true); // boolean true-pos/neg
-assert(false, posNeg, -4, 5, true); // boolean true-neg/pos
+assert_eq(true, posNeg, -3, 2, false); // non 1
+assert_eq(true, posNeg, -2, 3, false); // non 1
+assert_eq(false, posNeg, -1, -1, false); // both negative
+assert_eq(false, posNeg, 1, 1, false); // both positive
+assert_eq(false, posNeg, -2, -3, false); // both negative-non 1
+assert_eq(false, posNeg, 2, 3, false); // both positive-non 1
+assert_eq(false, posNeg, 4, 5, true); // boolean true-both positve
+assert_eq(false, posNeg, 4, -5, true); // boolean true-pos/neg
+assert_eq(false, posNeg, -4, 5, true); // boolean true-neg/pos
+
+// Note: Only include this line if running in node.
+export {};
