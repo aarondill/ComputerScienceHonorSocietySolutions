63489df7a65ca983a532c5333f4f37b8382bf5eb
aarondill <aaronsacks2006@gmail.com>
2023-11-10 15:43:18 -0600

    no more ts for you

--- src/lib/getSolutions.tsx
+++ src/lib/getSolutions.tsx
@@ -3,7 +3,7 @@ import fs from "fs/promises";
 import path from "path";
 export const PUBLIC_DIR = path.resolve("public");
 export const SOLUTIONS_DIR = path.join(PUBLIC_DIR, "solutions");
-export const CODE_EXTENSIONS = [".ts", ".js"];
+export const CODE_EXTENSIONS = [".js"];
 export const VIDEO_EXTENSIONS = [".mp4", ".webm"];
 export const SCREENSHOT_EXTENSIONS = [".png", ".jpg"];
 
