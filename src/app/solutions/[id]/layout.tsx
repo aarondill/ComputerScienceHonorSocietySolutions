253a3d52c185d734a2f07e9e2d3ea3b50a138ddc
aarondill <aaronsacks2006@gmail.com>
2023-11-13 14:08:18 -0600

    fix: types in [id]/layout.tsx

--- src/app/solutions/[id]/layout.tsx
+++ src/app/solutions/[id]/layout.tsx
@@ -1,11 +1,11 @@
 import { getSolutions } from "@/lib/getSolutions";
 import { notFound } from "next/navigation";
 
-type Props = { params: { id: string }; searchParams: { [s: string]: string } };
-export default async function Layout({
-  children,
-  params,
-}: { children: React.ReactNode } & Props) {
+type Props = {
+  params: { id: string };
+  children: React.ReactNode;
+};
+export default async function Layout({ children, params }: Props) {
   const name = params.id;
   const sols = await getSolutions();
   if (!sols.includes(name)) return notFound();
