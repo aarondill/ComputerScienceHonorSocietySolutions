a4ea87d16ce953f02d3a435d705468dc49b9d934
aarondill <aaronsacks2006@gmail.com>
2023-11-13 14:29:21 -0600

    feat: Add dark mode!
    Using: https://github.com/pacocoursey/next-themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Navigation from "@/components/Navigation";
 import type { Metadata } from "next";
+import { Providers } from "./providers";
 
 export default function RootLayout({
   children,
@@ -9,9 +10,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navigation
-          routes={{ "/": "Home", "/solutions": "Solutions" }}></Navigation>
-        {children}
+        <Providers>
+          <Navigation
+            routes={{ "/": "Home", "/solutions": "Solutions" }}></Navigation>
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,23 +1,16 @@
-"use client";
 import Link from "next/link";
 import navigationStyles from "./navigation.module.css";
-import { usePathname } from "next/navigation";
 
 export default function Navigation({
   routes,
 }: {
   routes: { [href: string]: string };
 }) {
-  const pathname = usePathname();
   return (
     <nav className={navigationStyles.navigation}>
       <ul>
         {Object.keys(routes).map(href => (
-          <li
-            key={href}
-            className={`${navigationStyles.navigation_item} ${
-              href === pathname ? navigationStyles.active : ""
-            }`}>
+          <li key={href} className={navigationStyles.navigation_item}>
             <Link href={href}>{routes[href]}</Link>
           </li>
         ))}
