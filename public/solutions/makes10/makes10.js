b143f381f74ecba917f7c822208ddabd4662acab
aarondill <aaronsacks2006@gmail.com>
2024-03-17 06:58:19 -0500

    fix: eslint errors in /public

--- public/solutions/makes10/makes10.js
+++ public/solutions/makes10/makes10.js
@@ -1,4 +1,8 @@
-/** Given 2 ints, a and b, return true if one of them is 10 or if their sum is 10. */
+/**
+ * @param {number} a
+ * @param {number} b
+ * Given 2 ints, a and b, return true if one of them is 10 or if their sum is 10.
+ */
 const makes10 = (a, b) => a === 10 || b === 10 || a + b === 10;
 
 export { makes10 };
