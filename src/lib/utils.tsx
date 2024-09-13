87cf6cafdf672327d8c8bb6c5bfd7d84fdb9d195
Aaron Dill <aaronsacks2006@gmail.com>
2024-09-13 08:28:20 -0500

    fix: clean eslintrc

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,10 +1,12 @@
 export function ensureError(value: unknown): Error {
   if (value instanceof Error) return value;
 
-  let stringified = "[Unable to stringify the thrown value]";
+  let stringified: string;
   try {
     stringified = JSON.stringify(value);
-  } catch {}
+  } catch {
+    stringified = "[Unable to stringify the thrown value]";
+  }
 
   const error = new Error(
     `This value was thrown as is, not through an Error: ${stringified}`
